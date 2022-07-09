e5ea0aef7f0ddaf1cbd0ed4129ef077316b91459
roconnell15 <richard.oconnell15@gmail.com>
2022-07-09 18:36:35 +0000

    Finalized snake, need to add retry button.

--- projects/snake/index.js
+++ projects/snake/index.js
@@ -1,53 +1,183 @@
-/* global $, sessionStorage */
+const canvas=document.getElementById('game');
+const ctx=canvas.getContext('2d');
+//increase snake size 
+class snakePart{
+constructor(x, y){
+    this.x=x;
+    this.y=y;
+}
 
-$(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
+}
+
+let speed=7;
+let tileCount=20; 
+
+let tileSize=canvas.clientWidth/tileCount-2;
+let headX=10;
+let headY=10;
+
+// array for snake parts
+const snakeParts=[];
+let tailLength=2;
+
+//initialize the speed of snake
+let xvelocity=0;
+let yvelocity=0;
+
+//draw apple
+let appleX=5;
+let appleY=5;
+
+//scores
+let score=0;
+
+// create game loop-to continously update screen
+function drawGame(){
+    changeSnakePosition();
+    // game over logic
+    let result=isGameOver();
+    if(result){// if result is true
+        return;
+    }
+    clearScreen();
+    drawSnake();
+    drawApple();
   
-function runProgram(){
-  ////////////////////////////////////////////////////////////////////////////////
-  //////////////////////////// SETUP /////////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
+    checkCollision()
+    drawScore();
+    setTimeout(drawGame, 1000/speed);//update screen 7 times a second
+}
+//Game Over function
+function isGameOver(){
+    let gameOver=false; 
+    //check whether game has started
+    if(yvelocity===0 && xvelocity===0){
+        return false;
+    }
+    if(headX<0){//if snake hits left wall
+        gameOver=true;
+    }
+    else if(headX===tileCount){//if snake hits right wall
+        gameOver=true;
+    }
+    else if(headY<0){//if snake hits wall at the top
+        gameOver=true;
+    }
+    else if(headY===tileCount){//if snake hits wall at the bottom
+        gameOver=true;
+    }
 
-  // Constant Variables
-  var FRAMES_PER_SECOND_INTERVAL = 1000 / 60;
-  
-  // Game Item Objects
+    //stop game when snake crush to its own body
 
-
-  // one-time setup
-  var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
-
-  ////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////// CORE LOGIC ///////////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
-
-  /* 
-  On each "tick" of the timer, a new frame is dynamically drawn using JavaScript
-  by calling this function and executing the code inside.
-  */
-  function newFrame() {
+     for(let i=0; i<snakeParts.length;i++){
+         let part=snakeParts[i];
+         if(part.x===headX && part.y===headY){//check whether any part of snake is occupying the same space
+             gameOver=true;
+             break; // to break out of for loop
+         }
+     }
     
 
-  }
-  
-  /* 
-  Called in response to events.
-  */
-  function handleEvent(event) {
+    //display text Game Over
+    if(gameOver){
+     ctx.fillStyle="white";
+     ctx.font="50px arial";
+     ctx.fillText("Game Over! ", canvas.clientWidth/6.5, canvas.clientHeight/2);//position our text in center
+    }
 
-  }
-
-  ////////////////////////////////////////////////////////////////////////////////
-  ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
-  ////////////////////////////////////////////////////////////////////////////////
-
-  
-  function endGame() {
-    // stop the interval timer
-    clearInterval(interval);
-
-    // turn off event handlers
-    $(document).off();
-  }
-  
+    return gameOver;// this will stop execution of drawgame method
 }
+
+// score function
+function drawScore(){
+ctx.fillStyle="white"// set our text color to white
+ctx.font="20px arial"//set font size to 10px of font family verdena
+ctx.fillText("Score: " +score, canvas.clientWidth-90,30);// position our score at right hand corner 
+
+}
+
+// clear our screen
+ function clearScreen(){
+
+ctx.fillStyle= 'black'// make screen black
+ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)// black color start from 0px left, right to canvas width and canvas height
+
+ }
+ function drawSnake(){
+    
+    ctx.fillStyle="lightgreen";
+    //loop through our snakeparts array
+    for(let i=0;i<snakeParts.length;i++){
+        //draw snake parts
+        let part=snakeParts[i]
+         ctx.fillRect(part.x *tileCount, part.y *tileCount, tileSize,tileSize)
+    }
+    //add parts to snake --through push
+    snakeParts.push(new snakePart(headX,headY));//put item at the end of list next to the head
+    if(snakeParts.length>tailLength){
+        snakeParts.shift();//remove furthest item from  snake part if we have more than our tail size
+
+    }
+    ctx.fillStyle="green";
+    ctx.fillRect(headX* tileCount,headY* tileCount, tileSize,tileSize)
+
+
+ }
+ function changeSnakePosition(){
+     headX=headX + xvelocity;
+     headY=headY+ yvelocity;
+     
+ }
+ function drawApple(){
+     ctx.fillStyle="red";
+     ctx.fillRect(appleX*tileCount, appleY*tileCount, tileSize, tileSize)
+ }
+ // check for collision and change apple position
+ function checkCollision(){
+     if(appleX==headX && appleY==headY){
+         appleX=Math.floor(Math.random()*tileCount);
+         appleY=Math.floor(Math.random()*tileCount);
+         tailLength++;
+         score++; //increase our score value
+
+     }
+ }
+ //add event listener to our body
+ document.body.addEventListener('keydown', keyDown);
+
+function keyDown()
+    //up
+{
+    if(event.keyCode==38){
+        //prevent snake from moving in opposite direcction
+        if(yvelocity==1)
+        return;
+        yvelocity=-1;
+        xvelocity=0;
+        
+    }
+    //down
+    if(event.keyCode==40){
+        if(yvelocity==-1)
+        return;
+        yvelocity=1;
+        xvelocity=0;
+    }
+
+    //left
+    if(event.keyCode==37){
+        if(xvelocity==1)
+        return;
+        yvelocity=0;
+        xvelocity=-1;
+    }
+    //right
+    if(event.keyCode==39){
+        if(xvelocity==-1)
+        return;
+        yvelocity=0;
+        xvelocity=1;
+    }
+}
+
+ drawGame(); 
