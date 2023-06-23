a9d5615353eec9ba8b9a761689b361be94b832a6
roconnell15 <richard.oconnell15@gmail.com>
2023-06-23 16:28:52 +0000

    New file?

--- asd-projects/sorting-exercise/index.js
+++ asd-projects/sorting-exercise/index.js
@@ -23,8 +23,11 @@ The CSS ids you will work with are:
 
 
 // TODO 1: Implement swap
-function swap(array, i, i){
-
+function swap(array, i, j){
+    var temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+    drawSwap(array, i, j);
 }
 
 ///////////////////////////////////////////////////////////////////////
