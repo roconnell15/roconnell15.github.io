058b2a006758578b8ca0a8f4f4d6a22ae98d306e
roconnell15 <richard.oconnell15@gmail.com>
2023-11-13 18:17:44 +0000

    Added ball, new touch fixes?

--- projects/soccer-manager/script.js
+++ projects/soccer-manager/script.js
@@ -4,6 +4,10 @@ els.forEach((name) => {
 });
 
 els.forEach((name) => {
+  dragElementNew(name);
+});
+
+els.forEach((name) => {
     returnElem(name);
   });
 
@@ -14,7 +18,6 @@ function dragElement(elmnt) {
     pos3 = 0,
     pos4 = 0;
   elmnt.onmousedown = dragMouseDown;
-  elmnt.ontouchstart = dragMouseDown;
 
   function dragMouseDown(e) {
     e = e || window.event;
@@ -23,10 +26,8 @@ function dragElement(elmnt) {
     pos3 = e.clientX;
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
-    document.ontouchend = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
-    document.ontouchmove = elementDrag;
   }
   function elementDrag(e) {
     e = e || window.event;
@@ -44,6 +45,45 @@ function dragElement(elmnt) {
     /* stop moving when mouse button is released:*/
     document.onmouseup = null;
     document.onmousemove = null;
+
+  }
+}
+
+
+function dragElementNew(elmnt) {
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  
+  elmnt.ontouchstart = dragTouchDown;
+
+  function dragTouchDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    
+    document.ontouchend = closeDragElementNew;
+    // call a function whenever the cursor moves:
+    
+    document.ontouchmove = elementDragNew;
+  }
+  function elementDragNew(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+    elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+  }
+  function closeDragElementNew() {
+    /* stop moving when mouse button is released:*/
 
     document.ontouchend = null;
     document.ontouchmove = null;
