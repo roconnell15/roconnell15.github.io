c9500230cc5dd8c2d6683477a8835ef916733a45
roconnell15 <richard.oconnell15@gmail.com>
2024-01-30 14:22:18 +0000

    Walker object update

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,19 +10,21 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  const KEYS = {
-    LEFT: 37,
-    RIGHT: 39,
-    UP: 38,
-    DOWN: 40
+  var KEYS = {
+    "LEFT": 37,
+    "RIGHT": 39,
+    "UP": 38,
+    "DOWN": 40
   }
 
 
   // Game Item Objects
-  var positionX = 0;
-  var positionY = 0;
-  var speedX = 0;
-  var speedY = 0;
+var walker = {   
+  "positionX" : 0,
+  "positionY" : 0,
+  "speedX" : 0,
+  "speedY" : 0,
+}
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -49,24 +51,24 @@ function runProgram(){
   function handleKeyDown(event) {
       if(event.which === KEYS.UP){
         console.log("UP PRESSED")
-        speedY = -5;
+        walker.speedY = -5;
       }
       if(event.which === KEYS.LEFT){
-        speedX = -5;
+        walker.speedX = -5;
         console.log("LEFT PRESSED")
       }
       if(event.which === KEYS.RIGHT){
         console.log("RIGHT PRESSED")
-        speedX = 5;
+        walker.speedX = 5;
       }
       if(event.which === KEYS.DOWN){
         console.log("DOWN PRESSED")
-        speedY = 5;
+        walker.speedY = 5;
       }
   }
     function handleKeyUp(){
-      speedX = 0;
-      speedY = 0;
+      walker.speedX = 0;
+      walker.speedY = 0;
     }
 
 
@@ -75,15 +77,15 @@ function runProgram(){
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
   function repositionGameItem(){
-    positionX += speedX;
-    positionY += speedY;
+    walker.positionX += walker.speedX;
+    walker.positionY += walker.speedY;
   }
 
   function redrawGameItem(){
-    $("#walker").css("left", positionX);
-    $("#walker").css("right", positionX);
-    $("#walker").css("top", positionY);
-    $("#walker").css("bottom", positionY);
+    $("#walker").css("left", walker.positionX);
+    $("#walker").css("right", walker.positionX);
+    $("#walker").css("top", walker.positionY);
+    $("#walker").css("bottom", walker.positionY);
   }
   
   function endGame() {
