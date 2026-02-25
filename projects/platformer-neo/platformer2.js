525396a6273e0019bf6521b99fa7dd11488440cd
roconnell15 <108817083+roconnell15@users.noreply.github.com>
2026-02-25 16:24:35 +0000

    added second level to platformer

---
+++ projects/platformer-neo/platformer2.js
@@ -0,0 +1,81 @@
+$(function () {
+  // initialize canvas and context when able to
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
+  window.addEventListener("load", loadJson);
+
+  function setup() {
+    if (firstTimeSetup) {
+      halleImage = document.getElementById("player");
+      projectileImage = document.getElementById("projectile");
+      cannonImage = document.getElementById("cannon");
+      $(document).on("keydown", handleKeyDown);
+      $(document).on("keyup", handleKeyUp);
+      firstTimeSetup = false;
+      //start game
+      setInterval(main, 1000 / frameRate);
+    }
+    //create walls
+    createPlatform(-50, -50, canvas.width + 100, 50); //top
+    createPlatform(-50, canvas.height - 10, canvas.width + 100, 200); //right
+    createPlatform(-50, -50, 50, canvas.height + 500); //bottom
+    createPlatform(canvas.width, -50, 50, canvas.height + 100);
+
+    /////////////////////////////////////////////////
+    //////////ONLY CHANGE BELOW THIS POINT///////////
+    /////////////////////////////////////////////////
+
+    // TODO 1
+    // Create platforms
+    // You must decide the x position, y position, width, and height of the platforms
+    // example usage: createPlatform(x,y,width,height)
+
+    createPlatform(200, 620, 290, 20, "pink");
+    createPlatform(90, 500, 200, 20, "pink");
+    createPlatform(500, 400, 200, 20, "pink");
+    createPlatform(667, 280, 300, 20, "pink");
+    createPlatform(900, 450, 20, 290, "pink");
+    createPlatform(1200, 550, 100, 20, "pink");
+    createPlatform(750, 620, 70, 20, "beige");
+    createPlatform(830, 500, 70, 20, "beige");
+    createPlatform(1100, 620, 70, 20, "beige");
+    
+    
+    
+    
+    // TODO 2
+    // Create collectables
+    // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
+    // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
+    // example usage: createCollectable("type", x, y, gravity, bounce)
+    
+    createCollectable("database", 220, 400, 1, 1);
+   
+    
+    
+    
+    // TODO 3
+    // Create cannons
+    // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
+    // Your wall choices are: 'top' 'left' 'right' and 'bottom'
+    // example usage: createCannon("side", position, delay)
+
+    
+    createCannon("top", 500, 2000)
+    createCannon("top", 900, 700)
+    createCannon("right", 425, 1000)
+    createCollectable("diamond", 1010, 600, 0, 0);
+    createCollectable("diamond", 900, 600, 0, 0);
+    createCollectable("diamond", 800, 600, 0, 0);
+    createCollectable("diamond", 700, 600, 0, 0);
+
+
+
+
+    /////////////////////////////////////////////////
+    //////////ONLY CHANGE ABOVE THIS POINT///////////
+    /////////////////////////////////////////////////
+  }
+
+  registerSetup(setup);
+});
